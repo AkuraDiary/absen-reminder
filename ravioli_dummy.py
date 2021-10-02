fea71d1da81c9c895e10f3885e9f9d4dda7444b1
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-10-02 08:23:17 +0700

    first commit

---
+++ ravioli_dummy.py
@@ -0,0 +1,9 @@
+import absen_reminder
+def main():
+    print("Ravioli dummy called")
+    print("Waktu Absen ? ", absen_reminder.waktu_absen())
+    input("Press anything to close")
+
+if __name__ == "__main__":
+    main()
+    
